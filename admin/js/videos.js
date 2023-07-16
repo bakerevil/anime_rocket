2248c62164d4b2386a142019ff395d2bb4a3ea03
CarlosEstrella <43283948+CarlosEstrella@users.noreply.github.com>
2023-07-15 22:34:56 -0500

    cambios en videos

--- admin/js/videos.js
+++ admin/js/videos.js
@@ -19,7 +19,7 @@ function get_data () {
                                 <th>
                                     <input type="checkbox" value="${elemento.id}"  class="checkboxes">
                                 </th>
-                                <td>${elemento.titulo}</td>
+                                <td>${elemento.capitulo}</td>
                                 <td>${elemento.foto}</td>
                                 <td>${elemento.video}</td>
                                 <td>${elemento.categoria}</td>
@@ -50,112 +50,112 @@ btnNew.addEventListener("click", (event) => {
     event.preventDefault()
     showForm()
 })
-// btnSave.addEventListener("click", (event) => {
-//     event.preventDefault()
-//     if (titulo.value != "" && foto.value != "" && video.value != "" && categoria.value != "" && listas.value != "" && fecha_insertada.value != "" && fecha_publicada.value != ""  && statuses.value != "" ){
-//         let formdata = new FormData(form)
-//         formdata.append("funcion", "insert_data")
-//         if (btnSave.hasAttribute("data-id")) {
-//             formdata.set("funcion", "update_data")
-//             formdata.append("id", btnSave.getAttribute("data-id"))
-//         }
-//         opciones.body = formdata
-//         fetch('consultav.php', opciones)
-//             .then(respuesta => respuesta.json())
-//             .then(resultado => {
-//                 alert(resultado.text)
-//                 if (resultado.status == "success") {
-//                     data.style.display = "block"
-//                     form.reset()
-//                     insert_data.style.display = "none"
-//                     get_data()
-//                     btnSave.innerText = "Guardar"
-//                     btnSave.removeAttribute("data-id")
-//                 }
-//             })
-//     }
-// })
-// let refresh = document.getElementById('refresh');
-// refresh.addEventListener('click', _ => {
-//             location.reload();
-//             console.log('refresh')
-// })
-// SelectAll.addEventListener("change", checkbox => {
-//     const checkboxes = document.querySelectorAll(".checkboxes")
-//     checkboxes.forEach(element => {
-//         element.checked = false
-//     })
-//     if (SelectAll.checked) {
-//         checkboxes.forEach(element => {
-//             element.checked = true
-//         })
-//     }
-//     showDeleteIcon()
-// })
-// const isSelected = item => item.checked
-// const showDeleteIcon = () => {
-//     const checkboxes = document.querySelectorAll(".checkboxes")
-//     const arrayCheckboxes = Array.from(checkboxes)
-//     const someChecked = arrayCheckboxes.some(isSelected)
-//     btnBorrar.style.display = "none"
-//     if (someChecked) {
-//         btnBorrar.style.display = "inline-block"
-//     }
-// }
-// tabla.addEventListener("click", event => {
-//     if (event.target.classList.contains('btn_editar')) {
-//         event.preventDefault()
-//         showForm()
-//         const formData = new FormData()
-//         const id = event.target.getAttribute('data-id')
-//         formData.append("funcion", "get_one")
-//         formData.append("id", id)
-//         fetch("consultav.php", {
-//             method: "POST",
-//             body: formData
-//         })
-//             .then(response => response.json())
-//             .then(row => {
-//                 titulo.value = row.titulo
-//                 foto.value = row.foto
-//                 video.value = row.video
-//                 categoria.value = row.categoria
-//                 listas.value = row.listas
-//                 fecha_insertada.value = row.fecha_insertada
-//                 fecha_publicada.value = row.fecha_publicada
-//                 orden.value = row.orden
-//                 statuses.value = row.status
-//                 btnSave.setAttribute("data-id", row.id)
-//                 btnSave.innerText = "Editar"
-//             })
-//     }
-//     if (event.target.classList.contains('checkboxes')) {
-//         showDeleteIcon()
-//     }
-// })
+btnSave.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (titulo.value != "" && foto.value != "" && video.value != "" && categoria.value != "" && listas.value != "" && fecha_insertada.value != "" && fecha_publicada.value != ""  && statuses.value != "" ){
+        let formdata = new FormData(form)
+        formdata.append("funcion", "insert_data")
+        if (btnSave.hasAttribute("data-id")) {
+            formdata.set("funcion", "update_data")
+            formdata.append("id", btnSave.getAttribute("data-id"))
+        }
+        opciones.body = formdata
+        fetch('consultav.php', opciones)
+            .then(respuesta => respuesta.json())
+            .then(resultado => {
+                alert(resultado.text)
+                if (resultado.status == "success") {
+                    data.style.display = "block"
+                    form.reset()
+                    insert_data.style.display = "none"
+                    get_data()
+                    btnSave.innerText = "Guardar"
+                    btnSave.removeAttribute("data-id")
+                }
+            })
+    }
+})
+let refresh = document.getElementById('refresh');
+refresh.addEventListener('click', _ => {
+            location.reload();
+            console.log('refresh')
+})
+SelectAll.addEventListener("change", checkbox => {
+    const checkboxes = document.querySelectorAll(".checkboxes")
+    checkboxes.forEach(element => {
+        element.checked = false
+    })
+    if (SelectAll.checked) {
+        checkboxes.forEach(element => {
+            element.checked = true
+        })
+    }
+    showDeleteIcon()
+})
+const isSelected = item => item.checked
+const showDeleteIcon = () => {
+    const checkboxes = document.querySelectorAll(".checkboxes")
+    const arrayCheckboxes = Array.from(checkboxes)
+    const someChecked = arrayCheckboxes.some(isSelected)
+    btnBorrar.style.display = "none"
+    if (someChecked) {
+        btnBorrar.style.display = "inline-block"
+    }
+}
+tabla.addEventListener("click", event => {
+    if (event.target.classList.contains('btn_editar')) {
+        event.preventDefault()
+        showForm()
+        const formData = new FormData()
+        const id = event.target.getAttribute('data-id')
+        formData.append("funcion", "get_one")
+        formData.append("id", id)
+        fetch("consultav.php", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => response.json())
+            .then(row => {
+                titulo.value = row.capitulo
+                foto.value = row.foto
+                video.value = row.video
+                categoria.value = row.categoria
+                listas.value = row.anime
+                fecha_insertada.value = row.fecha_insertada
+                fecha_publicada.value = row.fecha_publicada
+                orden.value = row.orden
+                statuses.value = row.status
+                btnSave.setAttribute("data-id", row.id)
+                btnSave.innerText = "Editar"
+            })
+    }
+    if (event.target.classList.contains('checkboxes')) {
+        showDeleteIcon()
+    }
+})
 
-// btnBorrar.addEventListener("click", event => {
-//     event.preventDefault()
-//     const confirmDelete = confirm("Estás seguro de borrar los datos?")
-//     if (confirmDelete) {
-//         const checkboxes = document.querySelectorAll(".checkboxes")
-//         const arrayCheckboxes = Array.from(checkboxes)
-//         const itemsCheckbox = arrayCheckboxes
-//             .filter(checkbox => checkbox.checked)
-//             .map(checkbox => checkbox.value)
+btnBorrar.addEventListener("click", event => {
+    event.preventDefault()
+    const confirmDelete = confirm("Estás seguro de borrar los datos?")
+    if (confirmDelete) {
+        const checkboxes = document.querySelectorAll(".checkboxes")
+        const arrayCheckboxes = Array.from(checkboxes)
+        const itemsCheckbox = arrayCheckboxes
+            .filter(checkbox => checkbox.checked)
+            .map(checkbox => checkbox.value)
 
-//         const formData = new FormData()
-//         formData.append("funcion", "delete_data")
-//         formData.append("data", itemsCheckbox)
-//         fetch("consulta.php", {
-//             method: "POST",
-//             body: formData
-//         })
-//             .then(response => response.json())
-//             .then(json => {
-//                 if (json.status == "success") {
-//                     get_data()
-//                 }
-//             })
-//     }
-// })
+        const formData = new FormData()
+        formData.append("funcion", "delete_data")
+        formData.append("data", itemsCheckbox)
+        fetch("consultarv.php", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => response.json())
+            .then(json => {
+                if (json.status == "success") {
+                    get_data()
+                }
+            })
+    }
+})
