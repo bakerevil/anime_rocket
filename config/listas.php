6e141a7e146951f9d1f34045d0f5edd30522c01c
Andrik2406 <93060312+Andrik2406@users.noreply.github.com>
2023-07-16 22:03:35 -0500

    correccion de listas
    se corrigio errores

--- config/listas.php
+++ config/listas.php
@@ -3,11 +3,11 @@ class listas extends mysqli
 {
     public function _construct($host, $user, $pass, $db)
     {
-        parent ::_construct($host, $user, $pass, $db);
+        parent ::__construct($host, $user, $pass, $db);
     }
 
     public function get_listas(){
-        $consulta = "SELECT * FROM listas";
+        $consulta = "SELECT s.id, s.titulo, s.thumbnail, s.capitulos, s.fecha_insercion, s.votos, s.anio, s.l_tipo, s.l_categoria, c.rv_status FROM listas s LEFT JOIN rv_status c ON s.l_status = c.rv_id ";
         $query=$this->query($consulta);
         return $query;
     }
--- directorio.php
+++ directorio.php
@@ -43,7 +43,7 @@
         <p class="icon">►</p>
         <img src="<?php echo $row ['thumbnail']; ?>" alt="">
         <div class="episode_description">
-        <h3 class="episode_lis"><?php echo $row['l_status'];?></h3>
+        <h3 class="episode_lis"><?php echo $row['rv_status'];?></h3>
           <p class="episode_number">ANIME <?php echo $row ['id'];?></p>
           <h3 class="episode_title"><?php echo $row ['titulo'];?></h3>
         </div>
