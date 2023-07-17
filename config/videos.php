4009ce450faac228e9b42662d644f026cac7da40
bakerevil <92763889+bakerevil@users.noreply.github.com>
2023-07-16 20:41:06 -0500

    agregado de la barra colapsar
    se agrego la funcion en js para que la barra lateral se oculte y unos arreglos al codigo en geneal

--- config/videos.php
+++ config/videos.php
@@ -9,7 +9,7 @@ class videos extends mysqli
     }
 
     public function get_videos(){
-        $consulta = "SELECT v.id, v.titulo, v.thumbnail, v.archivo, c.categoria, v.lista, v.fecha_insercion, v.orden, v.v_status FROM videos v INNER JOIN rv_categoria c ON v.categoria = c.id";
+        $consulta = "SELECT v.id, v.capitulo, v.thumbnail, v.archivo, c.categoria, v.anime, v.fecha_insercion, v.orden, v.v_status FROM videos v INNER JOIN rv_categoria c ON v.categoria = c.id";
         $query=$this->query($consulta);
         return $query;
     }
