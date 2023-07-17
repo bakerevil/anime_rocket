bb5b055cdbaa3b010a9679e4923a89f50ab2ecff
David Isai <91503790+Davidav18@users.noreply.github.com>
2023-07-16 23:27:42 -0600

    Roles
    Cambios en el login y en el valid

--- config/valid.php
+++ config/valid.php
@@ -1,10 +1,24 @@
 <?php
-    $validation = new Admin("localhost","root","","anime_rocket");
-    $result = $validation -> validation($_POST['correo'],$_POST['passwords']);
-        if (mysqli_num_rows($result) > 0 ) {
-            session_start();
-            header("location: ../admin/modules/usuarios/index.html");
-        } else{
-            header("location: ../admin/inicio.php");
-        }
+require_once('valid.php');
+
+$validation = new Admin("localhost", "root", "", "anime_rocket");
+$result = $validation->validation($_POST['correo'], $_POST['passwords']);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $user_role = $row['rol'];
+
+    session_start();
+
+    // Redirecciona automáticamente según el rol
+    if ($user_role === '1') {
+        header("location: ../admin/modules/usuarios/index.html");
+    } elseif ($user_role === '0') {
+        header("location:../../ANIME_ROCKET/index.php");
+    } else {
+        header("location: ANIME_ROCKET/admin/inicio.php");
+    }
+} else {
+    header("location: ../admin/inicio.php");
+}
 ?>
