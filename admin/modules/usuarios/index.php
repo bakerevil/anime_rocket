1e319ff55172ec5f1d8eff6ff57ce6650db6b143
LizEuan08 <118142066+LizEuan08@users.noreply.github.com>
2023-07-30 00:25:48 -0500

    Solucionando_issues
    Solución a  los issues 24, 27, 29 y 45

--- admin/modules/usuarios/index.php
+++ admin/modules/usuarios/index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <title>Document</title>
+    <title>USUARIOS</title>
 </head>
 
 <body>
