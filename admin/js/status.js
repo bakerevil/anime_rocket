c159f395e139840e71e1417b43ae0ddb730d7758
Leonardo Garcia <92763889+bakerevil@users.noreply.github.com>
2023-07-31 19:32:14 -0600

    sidebar y chevron
    arreglo de sidebar y el chevron se puso lugar

--- admin/js/status.js
+++ admin/js/status.js
@@ -3,48 +3,7 @@ const tabla = document.querySelector('#cuerpo');
 let opciones = {
     method: 'POST'
 }
-
-let estado = "open";
-function showHideSideBar(){
-    
-    var links = document.getElementsByClassName("txtLink");
-    if (estado=="open") {
-        let angle = 0,
-    img = document.getElementById('img_container');
-    document.getElementById('img_container').onclick = function() {
-    angle = (angle + 180) % 360;
-    img.className = "rotate" + angle;
-    }
-        document.getElementById("sideBar").style.width="80px";
-        document.getElementById("main").style.marginLeft="80px";
-        document.getElementById("img-info").style.width="50px";
-        document.getElementById("texto-info").style.display="none";
-        for (var i = links.length - 1; i >= 0; i--){
-            links[i].style.display = "none"
-        }
-        estado ="close";
-    }
-    else if (estado=="close") {
-        let angle = 180,
-    img = document.getElementById('img_container');
-    document.getElementById('img_container').onclick = function() {
-    angle = (angle - 180) % 360;
-    img.className = "rotate" + angle;
-    }
-        document.getElementById("sideBar").style.width="250px";
-        document.getElementById("main").style.marginLeft="250px";
-        document.getElementById("img-info").style.width="112px";
-        document.getElementById("texto-info").style.display="block";
-
-        for (var i = links.length - 1; i >= 0; i--){
-            links[i].style.display = "inline"
-        }
-        estado ="open";
-    }
-    
-}
 let formdata = new FormData()
-
 function get_data () {
     formdata.append("funcion", "get_data")
     opciones.body = formdata
