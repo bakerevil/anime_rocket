279f7101187a8a3aaef3e8342e63220ff42cf4c6
Jejiro53 <90729386+Jejiro53@users.noreply.github.com>
2023-08-01 18:03:07 -0500

    Modulo de Categoria
    Se realizo el apartado de Categorias

---
+++ admin/modules/categoria/consultass.php
@@ -0,0 +1,112 @@
+<?php
+class modules extends mysqli
+{
+    public function __construct($host, $usuario, $pass, $bd)
+    {
+        parent::__construct($host, $usuario, $pass, $bd);
+    }
+
+    public function get_data()
+    {
+        $consulta = "SELECT * FROM rv_categoria";
+        $result = mysqli::query($consulta);
+        $array = [];
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $array[] = [
+                "id"=> $row["id"],
+                "categorias"=> $row["categoria"],
+            ];
+        }
+        echo json_encode($array);
+    }
+    public function get_one($id)
+    {
+        $consulta = "SELECT * FROM rv_categoria  WHERE id = $id";
+        $result = mysqli::query($consulta);
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        $array = [
+            "id"=> $row["id"],
+                "categorias"=> $row["categoria"],
+        ];
+        echo json_encode($array);
+    }
+
+    public function insert_data()
+    {
+        mysqli_report(MYSQLI_REPORT_OFF);
+        
+        $categoria = $_POST['categorias'];
+
+        $consulta = "INSERT INTO rv_categoria (categoria) VALUES ('$categoria')";
+        $result = mysqli::query($consulta);
+        if ($result) {
+            $array = [
+            "status" => "success",
+            "text" => "Se insertó correctamente"
+        ];
+        }else{
+            $array = [
+                "status" => "error",
+                "text" => "No se pudo insertar el registro"
+            ];
+        }
+        echo json_encode($array);
+    }
+    
+    public function update_data()
+    {
+        mysqli_report(MYSQLI_REPORT_OFF);
+        $id = $_POST['id'];
+        $categoria = $_POST['categorias'];
+         
+        $consulta = "UPDATE rv_categoria set categoria = '$categoria' WHERE id =  $id";
+        $array = [
+            "status" => "success",
+            "text" => "Se editó correctamente"
+        ];
+
+        if (!mysqli::query($consulta)) {
+            $array = [
+                "status" => "error",
+                "text" => "No se pudo insertar el registro"
+            ];
+        }
+        echo json_encode($array);
+    }
+    public function delete_data()
+    {
+        $datos = $_POST["data"];
+        $consulta = "DELETE FROM rv_categoria WHERE id IN ($datos)";
+        mysqli::query($consulta);
+        $array = [
+            "text" => "Se eliminó correctamente",
+            "status" => "success",
+        ];
+        echo json_encode($array);
+    }
+}
+
+$modules = new modules("localhost", "root", "", "anime_rocket");
+
+if (isset($_POST)) {
+    switch ($_POST["funcion"]) {
+        case 'get_data':
+            $modules->get_data();
+            break;
+        case 'get_one':
+            $modules->get_one($_POST['id']);
+            break;
+        case 'insert_data':
+            $modules->insert_data();
+            break;
+        case 'update_data':
+            $modules->update_data($_POST['id']);
+            break;
+        case 'delete_data':
+            $modules->delete_data();
+            break;
+        default:
+            echo "Función incompleta";
+            break;
+    }
+}
