e31427e7c91ce05a663a97fce10636ae97b7ed4c
Leonardo Garcia <92763889+bakerevil@users.noreply.github.com>
2023-08-08 18:40:54 -0600

    style
    areglo de la imagen y el texto

--- admin/js/sidebar.js
+++ admin/js/sidebar.js
@@ -14,6 +14,7 @@ function showHideSideBar(){
     document.getElementById("sideBar").style.width="80px";
     document.getElementById("main").style.marginLeft="80px";
     document.getElementById("imgInfo").style.width="50px";
+    document.getElementById("imgInfo").style.height="50px";
     document.getElementById("texto-info").style.display="none";
     for (var i = links.length - 1; i >= 0; i--){
         links[i].style.display = "none"
@@ -30,6 +31,7 @@ function showHideSideBar(){
     document.getElementById("sideBar").style.width="250px";
     document.getElementById("main").style.marginLeft="250px";
     document.getElementById("imgInfo").style.width="112px";
+    document.getElementById("imgInfo").style.height="112px";
     document.getElementById("texto-info").style.display="block";
     for (var i = links.length - 1; i >= 0; i--){
         links[i].style.display = "inline"
