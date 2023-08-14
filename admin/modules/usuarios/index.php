cfed8def12c835c59a0e396e846c4c55ea819ef1
Andrik2406 <93060312+Andrik2406@users.noreply.github.com>
2023-08-14 12:44:29 -0500

    usuarios
    resolvi problemas de deprecacion en la tabla junto con el insertar de las imagenes

--- admin/modules/usuarios/index.php
+++ admin/modules/usuarios/index.php
@@ -41,6 +41,7 @@
                         <th> Nombre</th>
                         <th> Rol</th>
                         <th> Status</th>
+                        <th> avatar</th>
                         <th> Editar</th>
                     </tr>
                 </thead>
