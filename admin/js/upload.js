d2300a0eec614b6974d566cdbc5d3d4eb2e5826d
CarlosEstrella <43283948+CarlosEstrella@users.noreply.github.com>
2023-08-18 18:13:14 -0500

    sadd

--- admin/js/upload.js
+++ admin/js/upload.js
@@ -25,3 +25,22 @@ fetch(php[module],{
     alert (json.text);
   })
 });
+
+video.addEventListener('change',(files) =>{
+  const fileUpload = files.target.files[0]
+  const form = new FormData()
+  form.append ("file" , fileUpload)
+  form.append("funcion", "set_video")
+  fetch(php[module],{
+      method: "POST",
+      body: form
+  
+      }).then(response => response.json())
+      .then(json => {
+        if (json.status == 'succes'){
+            videoprev.value = json.file
+            videoPreview.setAttribute("src", "../../../public/" + json.file)
+        }
+        alert (json.text);
+      })
+    });
